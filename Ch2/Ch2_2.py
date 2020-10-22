c7c4d686ffbabad7f68c231606ecc61375c8d0df
Seung Soo Lee <dltmdtn0128@gmail.com>
2020-10-22 19:28:19 +0900

    변수/자료형

---
+++ Ch2/Ch2_2.py
@@ -0,0 +1,22 @@
+a= "3.5"
+b= 4
+print(a*b)
+#result :3.53.53.53.5
+
+a= "3.5"
+b="1.5"
+print(a+b)
+#result :3.51.5
+
+a="3"
+b=float(a)
+print(b**int(a))
+#result :27.0
+
+print(b == int(a))
+#result : True
+
+a='20'
+b='4'
+print(type(float(a/b)))
+#result :Syntax error
