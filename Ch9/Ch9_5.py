6c01c184f35086ca98f77b00e336f179c193d3f8
Seung Soo Lee <dltmdtn0128@gmail.com>
2020-11-11 23:40:26 +0900

    lambda, mapreduce, 가변인자,선형대수학

--- Ch9/Ch9_5.py
+++ Ch9/Ch9_5.py
@@ -1,7 +1,19 @@
 from functools import reduce
-def matrix_mul(x,y):
-    return[[sum(a*b for a,b in zip(row_a,col_b)) for col_b in zip(*y)]for row_a in x]
-matrix_x = [[1,2,3],[4,5,6]]
-matrix_y = [[1,2],[3,4],[5,6]]
 
-print(matrix_mul(matrix_x,matrix_y))
+
+def matrix_mul(x, y):
+    return [[sum(a * b for a, b in zip(row_a, col_b)) for col_b in zip(*y)] for row_a in x]
+
+
+matrix_x = [[1, 2, 3], [4, 5, 6]]
+matrix_y = [[1, 2], [3, 4], [5, 6]]
+
+print(matrix_mul(matrix_x, matrix_y))
+
+
+def matrix_mul_1(*args):
+    mul = lambda x, y: [[sum(a * b for a, b in zip(row_x, column_y)) for column_y in zip(*y)] for row_x in x]
+    print(reduce(mul, args))
+
+
+print(matrix_mul_1(matrix_x, matrix_y))
